9ef81c9299ac8b5c5b90a302c686452abae85c1d
AimerAttack <wcpwcp9@gmail.com>
2023-04-20 11:42:53 +0800

    init commit

---
+++ Assets/Scripts/Entry.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Bright.Serialization;
+using SimpleJSON;
+using UnityEngine;
+
+public class Entry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadConfig();
+    }
+
+    void LoadConfig()
+    {
+        var tablesCtor = typeof(cfg.Tables).GetConstructors()[0];
+        var loaderReturnType = tablesCtor.GetParameters()[0].ParameterType.GetGenericArguments()[1];
+        // 根据cfg.Tables的构造函数的Loader的返回值类型决定使用json还是ByteBuf Loader
+        System.Delegate loader = loaderReturnType == typeof(ByteBuf) ?
+            new System.Func<string, ByteBuf>(LoadByteBuf)
+            : (System.Delegate)new System.Func<string, JSONNode>(LoadJson);
+        var tables = (cfg.Tables)tablesCtor.Invoke(new object[] {loader});
+        var reward = tables.TbReward.Get(1);
+        Debug.Log(reward.ToString());
+    }
+    
+    private  JSONNode LoadJson(string file)
+    {
+        return JSON.Parse(File.ReadAllText($"{Application.dataPath}/../GenerateDatas/json/{file}.json", System.Text.Encoding.UTF8));
+    }
+
+    private  ByteBuf LoadByteBuf(string file)
+    {
+        return new ByteBuf(File.ReadAllBytes($"{Application.dataPath}/../GenerateDatas/bin/{file}.bytes"));
+    }
+}
